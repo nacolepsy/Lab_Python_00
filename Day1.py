008ef2058864eebe0f86f27bbe77860aa5d8c26c
nacolepsy <nacolepsy@nacolepsy-Aspire-5538.(none)>
2012-06-19 15:59:13 +0000

    My New Commit

---
+++ Day1.py
@@ -0,0 +1,29 @@
+print 'Michael Asare'
+print 'Hello World'
+
+a=95
+print 'The variable a contains:', a
+
+
+
+print "The Answer to 11/2 is:", 11/2
+
+print "The Answer to 11.0/2 is:", 11.0/2
+print "The Answer to 3 to the power of 27 is :", 3**27
+print "The Answer to (134533 + 6889980 -84849) to the power of 3 is :", (134533 + 6889980 -84849)**3
+
+print 'The average time per meter is :', (1000*7.9)/45
+
+
+frienddistance=(4.7)**2
+mydistance=(9.4)**2
+
+
+answer= frienddistance + mydistance
+
+print answer
+
+finanswer= answer**.5
+print finanswer
+
+print "The Shortest distance between me and my friend is:", finanswer*0.621371, "Miles"
